11a818cd8ebffd6147dd44a5225b8547c50b5b92
mamadou barry <barry30546@gmail.com>
2022-12-28 09:29:18 +0100

    change name of component

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from  '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { pageRendu } from './pageRendu/component-summary.component';
+import { pageFooter } from './pagefooter/component-footer.component';
+import { pageFormulaire } from './pageFormulaire/component-form.component';
+import { pageHeader } from './pageHeader/component-header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    pageHeader,
+    pageFooter,
+    pageFormulaire,
+    pageRendu
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
